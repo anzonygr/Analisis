43feaceadd37e0dc5f236e5f503631c6a84a26c1
anzonyg <50388025+anzonyg@users.noreply.github.com>
2019-05-19 12:07:54 -0600

    Add files via upload

--- colegio.sql
+++ colegio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-05-2019 a las 06:56:49
+-- Tiempo de generación: 19-05-2019 a las 20:07:06
 -- Versión del servidor: 10.1.39-MariaDB
 -- Versión de PHP: 7.3.5
 
@@ -38,8 +38,18 @@ CREATE TABLE `actividad` (
   `TITULO` varchar(40) COLLATE utf8_spanish_ci DEFAULT NULL,
   `DESCRIPCION` varchar(250) COLLATE utf8_spanish_ci DEFAULT NULL,
   `NOTA` int(11) DEFAULT NULL,
-  `FECHA_ENTREGA` date DEFAULT NULL
+  `FECHA_ENTREGA` varchar(10) COLLATE utf8_spanish_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `actividad`
+--
+
+INSERT INTO `actividad` (`COD_ACTIVIDAD`, `COD_ASIGNACION`, `COD_GRADO`, `COD_CURSO`, `COD_SECCION`, `COD_CATEDRATICO`, `TITULO`, `DESCRIPCION`, `NOTA`, `FECHA_ENTREGA`) VALUES
+(1, 2, 4, 1, 2, 1, 'TITULO', 'prueba', 20, '15/05/2019'),
+(2, 1, 4, 1, 1, 1, 'TAREA 2', 'PRUEBA', 1, '15/05/2019'),
+(3, 1, 4, 1, 1, 1, 'tarea 4', 'prueba 16-05-2019', 5, '16/05/2019'),
+(4, 1, 4, 1, 1, 1, 'tarea 5', 'test 5', 1, '16/05/2019');
 
 -- --------------------------------------------------------
 
@@ -67,18 +77,38 @@ CREATE TABLE `asignacion` (
 --
 
 INSERT INTO `asignacion` (`COD_ASIGNACION`, `COD_GRADO`, `COD_CURSO`, `COD_SECCION`, `COD_CATEDRATICO`, `COD_ESTUDIANTE`, `HORARIO`, `ZONA`, `PARCIAL_1`, `PARCIAL_2`, `EXAMEN_FINAL`, `ESTADO`) VALUES
-(1, 4, 1, 1, 1, 1, '15:30', 20, 7, 15, 40, 'asignado'),
-(2, 4, 2, 1, 1, 1, '15:30', 20, 7, 15, 40, 'asignado'),
-(3, 4, 3, 1, 1, 1, '15:30', 20, 7, 15, 40, 'asignado'),
-(4, 4, 4, 1, 1, 1, '15:30', 20, 7, 15, 40, 'asignado'),
-(5, 4, 1, 2, 1, 2, '15:30', 20, 7, 15, 40, 'asignado'),
-(6, 4, 2, 2, 1, 2, '15:30', 20, 7, 15, 40, 'asignado'),
-(7, 4, 3, 2, 1, 2, '15:30', 20, 7, 15, 40, 'asignado'),
-(8, 4, 4, 2, 1, 2, '15:30', 20, 7, 15, 40, 'asignado'),
-(9, 4, 1, 1, 1, 1, '15:30', 0, 0, 0, 0, 'asignado'),
-(10, 4, 1, 1, 1, 1, '15:30', 0, 0, 0, 0, 'asignado'),
-(11, 4, 1, 2, 1, 1, '15:30', 0, 0, 0, 0, 'asignado'),
-(12, 4, 3, 2, 1, 1, '15:30', 0, 0, 0, 0, 'asignado');
+(1, 4, 1, 1, 1, 0, '15:30', 0, 0, 0, 0, 'asignado'),
+(2, 4, 1, 2, 1, 0, '15:30', 0, 0, 0, 0, 'asignado'),
+(3, 4, 2, 1, 1, 0, '15:30', 0, 0, 0, 0, 'asignado'),
+(4, 4, 2, 2, 1, 0, '15:30', 0, 0, 0, 0, 'asignado'),
+(5, 4, 3, 1, 1, 0, '15:30', 0, 0, 0, 0, 'asignado'),
+(6, 4, 3, 2, 2, 0, '15:30', 0, 0, 0, 0, 'asignado'),
+(7, 4, 4, 1, 2, 0, '15:30', 0, 0, 0, 0, 'asignado'),
+(8, 4, 4, 2, 2, 0, '15:30', 0, 0, 0, 0, 'asignado'),
+(9, 4, 1, 1, 1, 2, '15:30', 20, 7, 15, 40, 'asignado'),
+(10, 4, 2, 1, 1, 2, '15:30', 20, 7, 15, 40, 'asignado'),
+(11, 4, 3, 1, 1, 2, '15:30', 20, 7, 15, 40, 'asignado'),
+(12, 4, 4, 1, 2, 2, '15:30', 20, 7, 15, 40, 'asignado'),
+(13, 4, 1, 2, 1, 3, '15:30', 20, 7, 15, 40, 'asignado'),
+(14, 4, 2, 2, 1, 3, '15:30', 20, 7, 15, 40, 'asignado'),
+(15, 4, 3, 2, 2, 3, '15:30', 20, 7, 15, 40, 'asignado'),
+(16, 4, 4, 2, 2, 3, '15:30', 20, 7, 15, 40, 'asignado'),
+(17, 4, 1, 2, 1, 4, '15:30', 20, 7, 15, 40, 'asignado'),
+(18, 4, 2, 2, 1, 4, '15:30', 20, 7, 15, 40, 'asignado'),
+(19, 4, 3, 2, 2, 4, '15:30', 20, 7, 15, 40, 'asignado'),
+(20, 4, 4, 2, 2, 4, '15:30', 20, 7, 15, 40, 'asignado'),
+(21, 4, 1, 2, 1, 5, '15:30', 20, 7, 15, 40, 'asignado'),
+(22, 4, 2, 2, 1, 5, '15:30', 20, 7, 15, 40, 'asignado'),
+(23, 4, 3, 2, 2, 5, '15:30', 20, 7, 15, 40, 'asignado'),
+(24, 4, 4, 2, 2, 5, '15:30', 20, 7, 15, 40, 'asignado'),
+(25, 4, 1, 1, 1, 6, '15:30', 20, 7, 15, 40, 'asignado'),
+(26, 4, 2, 1, 1, 6, '15:30', 20, 7, 15, 40, 'asignado'),
+(27, 4, 3, 1, 1, 6, '15:30', 20, 7, 15, 40, 'asignado'),
+(28, 4, 4, 1, 2, 6, '15:30', 20, 7, 15, 40, 'asignado'),
+(29, 4, 1, 2, 1, 7, '15:30', 20, 7, 15, 40, 'asignado'),
+(30, 4, 2, 2, 1, 7, '15:30', 20, 7, 15, 40, 'asignado'),
+(31, 4, 3, 2, 2, 7, '15:30', 20, 7, 15, 40, 'asignado'),
+(32, 4, 4, 2, 2, 7, '15:30', 20, 7, 15, 40, 'asignado');
 
 -- --------------------------------------------------------
 
@@ -106,7 +136,8 @@ CREATE TABLE `catedratico` (
 --
 
 INSERT INTO `catedratico` (`COD_CATEDRATICO`, `COD_ROL`, `NOMBRE`, `APELLIDO`, `TELEFONO_1`, `TELEFONO_2`, `CORREO`, `DIRECCION`, `DPI`, `SEXO`, `FECHA_NACIMIENTO`, `TITULO`) VALUES
-(1, 2, 'CARLOS', 'RUIZ', 12345678, 87654321, 'cruiz@gmail.com', '16-45', '1234152630101', 'MASCULINO', '02/03/1978', 'PERITO CONTADOR');
+(1, 2, 'ANZONY', 'GONZALEZ', 12345678, 87654321, 'anzonyg@live.com', '16-45', '1234152630101', 'MASCULINO', '02/03/1978', 'INGENIERO EN SISTEMAS'),
+(2, 2, 'CARLOS', 'RUIZ', 12345678, 87654321, 'cruiz@gmail.com', '16-45', '1234152630101', 'MASCULINO', '02/03/1978', 'PERITO CONTADOR');
 
 -- --------------------------------------------------------
 
@@ -154,7 +185,8 @@ CREATE TABLE `encargado` (
 --
 
 INSERT INTO `encargado` (`COD_ENCARGADO`, `COD_ROL`, `NOMBRE`, `APELLIDO`, `TELEFONO_1`, `TELEFONO_2`, `CORREO`, `PARENTESCO`, `DIRECCION`, `DPI`) VALUES
-(1, 4, 'RAFAEL', 'GONZALEZ', 12345678, 87456123, 'rgonzalez@gmail.com', 'PADRE', '16-48', '1234152614501');
+(1, 4, 'RAFAEL', 'GONZALEZ', 12345678, 87456123, 'rgonzalez@gmail.com', 'PADRE', '16-48', '1234152614501'),
+(2, 4, 'DOUGLAS', 'OSORIO', 12345678, 87456123, 'dosorio@unesa.gob.gt', 'TIO', '16-48', '1234152614501');
 
 -- --------------------------------------------------------
 
@@ -176,6 +208,21 @@ CREATE TABLE `entrega_actividad` (
   `ESTADO` varchar(15) COLLATE utf8_spanish_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
+--
+-- Volcado de datos para la tabla `entrega_actividad`
+--
+
+INSERT INTO `entrega_actividad` (`COD_ENTREGA_ACTIVIDAD`, `COD_ACTIVIDAD`, `COD_ASIGNACION`, `COD_GRADO`, `COD_CURSO`, `COD_SECCION`, `COD_CATEDRATICO`, `COD_ESTUDIANTE`, `DESCRIPCION`, `NOTA`, `ESTADO`) VALUES
+(1, 1, 2, 4, 1, 2, 1, 3, '', 0, 'sin entregar'),
+(2, 1, 2, 4, 1, 2, 1, 4, '', 0, 'sin entregar'),
+(3, 1, 2, 4, 1, 2, 1, 5, '', 0, 'sin entregar'),
+(4, 2, 1, 4, 1, 1, 1, 2, '', 0, 'sin entregar'),
+(5, 2, 1, 4, 1, 1, 1, 6, '', 0, 'sin entregar'),
+(6, 3, 1, 4, 1, 1, 1, 2, '', 0, 'sin entregar'),
+(7, 3, 1, 4, 1, 1, 1, 6, '', 0, 'sin entregar'),
+(8, 4, 1, 4, 1, 1, 1, 2, '', 0, 'sin entregar'),
+(9, 4, 1, 4, 1, 1, 1, 6, '', 0, 'sin entregar');
+
 -- --------------------------------------------------------
 
 --
@@ -194,16 +241,22 @@ CREATE TABLE `estudiante` (
   `CUI` varchar(13) COLLATE utf8_spanish_ci DEFAULT NULL,
   `FECHA_NACIMIENTO` varchar(10) COLLATE utf8_spanish_ci DEFAULT NULL,
   `COD_GRADO` int(11) DEFAULT NULL,
-  `COD_ENCARGADO` int(11) DEFAULT NULL
+  `COD_ENCARGADO` int(11) DEFAULT NULL,
+  `CORREO` varchar(30) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `estudiante`
 --
 
-INSERT INTO `estudiante` (`COD_ESTUDIANTE`, `COD_ROL`, `NOMBRE`, `APELLIDO`, `TELEFONO_1`, `TELEFONO_2`, `SEXO`, `DIRECCION`, `CUI`, `FECHA_NACIMIENTO`, `COD_GRADO`, `COD_ENCARGADO`) VALUES
-(1, 3, 'ANZONY', 'GONZALEZ', 12345678, 12345678, 'MASCULINO', '16-78 2 av.', '6125126350101', '06/04/1988', 4, 1),
-(2, 3, 'ABIGAIL', 'GONZALEZ', 12345678, 12345678, 'FEMENINO', '16-96 4ta. calle', '6125126350101', '02/06/1978', 4, 1);
+INSERT INTO `estudiante` (`COD_ESTUDIANTE`, `COD_ROL`, `NOMBRE`, `APELLIDO`, `TELEFONO_1`, `TELEFONO_2`, `SEXO`, `DIRECCION`, `CUI`, `FECHA_NACIMIENTO`, `COD_GRADO`, `COD_ENCARGADO`, `CORREO`) VALUES
+(0, 3, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, ''),
+(2, 3, 'ANZONY', 'GONZALEZ', 87654321, 98712345, 'MASCULINO', '16-96 4ta. calle', '1234156230101', '02/06/1978', 4, 1, 'anzonyg@live.com'),
+(3, 3, 'ABIGAIL', 'GONZALEZ', 87654321, 12345678, 'FEMENINO', '16-96 4ta. calle', '1234152670101', '02/01/1989', 4, 1, ''),
+(4, 3, 'DANIEL', 'GOMEZ', 12345678, 12345678, 'MASCULINO', '3 av. 4 calle', '1234152670101', '02/06/1978', 4, 2, ''),
+(5, 3, 'LUIS', 'GOMEZ', 98765432, 12345678, 'MASCULINO', '3 av. 4 calle', '6125126350101', '02/01/1989', 4, 1, ''),
+(6, 3, 'MARIA', 'LOPEZ', 87654321, 98712345, 'FEMENINO', '16-78 2 av.', '1234152670101', '06/04/1988', 4, 2, ''),
+(7, 3, 'JOSE', 'GOMEZ', 87654321, 98712345, 'MASCULINO', '16-78 2 av.', '6125126350101', '02/01/1989', 4, 2, 'jgomez@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -251,9 +304,14 @@ CREATE TABLE `login` (
 
 INSERT INTO `login` (`COD_ROL`, `COD_LOGIN`, `USUARIO`, `PASSWORD`) VALUES
 (1, 1, 'anzonyg@live.com', '1234'),
-(3, 2, 'anzonyg@gmail.com', '123456'),
-(3, 4, 'abygonzalez@gmail.com', '123456'),
-(4, 1, 'rgonzalez@gmail.com', '123456');
+(2, 2, 'anzonyg@live.com', '12345'),
+(2, 3, 'cruiz@gmail.com', '12345'),
+(3, 6, 'anzonyg@live.com', '123456'),
+(3, 7, 'lgomez@gmail.com', '12345'),
+(3, 8, 'mlopez@gmail.com', '1234'),
+(3, 9, 'jgomez@gmail.com', '123456'),
+(4, 4, 'rgonzalez@gmail.com', '12345'),
+(4, 5, 'dosorio@unesa.gob.gt', '12345');
 
 -- --------------------------------------------------------
 
